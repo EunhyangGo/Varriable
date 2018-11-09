106681b8f593053cbdc11813826a2ce9cd221100
EunhyangGo <hyang2lol@naver.com>
2018-11-09 16:17:47 +0900

    first

---
+++ src/com/biz/var/Variable03.java
@@ -0,0 +1,34 @@
+package com.biz.var;
+
+public class Variable03 {
+public static void main(String[] args) {
+	
+	int intNum1;
+	int intNum2;
+	
+	intNum1 = 30 ;
+	intNum2 = intNum1; 
+	//intNum1의 값 30을 읽어서 intNum2에 저장하라 = 복사 개념
+
+	System.out.println("intNum1: " + intNum1); //30
+	System.out.println("intNum2: " + intNum2); //30
+	
+	intNum1 = 50;
+
+	System.out.println("intNum1: " + intNum1); //50
+	System.out.println("intNum2: " + intNum2); //50
+	
+	intNum1 = intNum1 + 10; 
+	// 1. intNum1에 저장된 50을 CPU로 보내고 
+	// 2. 10을 CPU로 보내고
+	// 3. 두 값을 + 연산하고 
+	// 4. 그 결과를 다시  intNum1에 저장
+	// ==> intNum1에 10을 증가, 누적
+	intNum1 = intNum1 + 10;
+	intNum1 = intNum1 + 10;
+	intNum1 = intNum1 + 10;
+	
+	System.out.println("Last: " + intNum1);
+	
+}
+}
